ff0e72672ec7f42904d40b6a7aecfa7de03362e8
Seungyeon Ryu <stbu4158@gmail.com>
2022-04-04 19:03:45 +0900

    revise comment

--- Algorithm/Week1/1-14.py
+++ Algorithm/Week1/1-14.py
@@ -1,14 +1,11 @@
 num = int(input("*를 출력합니다. \n몇개를 출력할까요?: "))
 space = int(input("몇 개마다 줄바꿈할까요?: "))
 
-# 별 갯수 = 10
-# 스페이스값 = 3
 
 for i in range(num):
-    # 인덱스는 0부터 시작하기 때문에 배수 성질에다가 -1해 줌
     print("*", end="")
     if i % space == space - 1:
         print()
 
-if num % space:
-    print()
+# 문제점: for문 반복 n번
+# if 판단도 n번 수행
