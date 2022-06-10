76da4008ec079d0511174216140ea0f0b594b4ba
Seungyeon Ryu <stbu4158@gmail.com>
2022-06-10 23:38:31 +0900

    remove garbage files

--- Algorithm/Week1/1-20.py
+++
@@ -1,4 +0,0 @@
-# 리스트를 활용하여 건너뛰는 값을 제외한 범위 생성
-
-for i in list(range(1, 8)) + list(range(9, 13)):
-    print(i, end=" ")
--- Algorithm/Week1/1-22.py
+++
@@ -1,8 +0,0 @@
-# 왼쪽 아래가 직각이등변인 삼각형 *로 출력
-
-num = int(input("왼쪽 아래가 직각인 이등변 삼각형을 출력합니다.\n짧은 변의 길이를 입력하세요.: "))
-
-for i in range(num):
-    for j in range(i+1):
-        print("*", end="")
-    print(" ")
